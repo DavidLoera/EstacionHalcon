2d020fc580dc489a23bc34ee62bbde175bbc4e11
DavidLoera <86201527+DavidLoera@users.noreply.github.com>
2021-08-30 13:36:24 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://davidloera:Upopo200@cluster0-daidy.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://user:pass@cluster0-daidy.mongodb.net/test?retryWrites=true&w=majority', {
 	useCreateIndex: true,
 	useNewUrlParser: true,
 	useFindAndModify: false
